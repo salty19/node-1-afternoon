9f26e6eee924c32e36cfe6e1c440c45ac533164c
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-24 15:11:00 -0700

    need to find error

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const products = require('../products.json')
+const getProducts = require('./getProducts')
+
+const app = express()
+const port = 3001
+
+app.get('/api/products', getProducts)
+app.get('api/product/:id', getProducts)
+
+app.listen(port, () => {
+    console.log(`Server LISTENING on port: ${port}`)
+})
