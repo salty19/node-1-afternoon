74e113f06026f0292209babf49fad75aff2e6a4c
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-24 17:03:56 -0700

    nope-iBrokeItAgain

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const getProducts = require('./getProducts')
 const app = express()
 const port = 3001
 
-app.get(`/api/products`, (req, res) => {
-    res.status(200).send(products)
-})
+app.get(`/api/products`, getProducts)
 
-app.listen(port, getProducts)
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+})
