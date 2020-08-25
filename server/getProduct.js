806626b5068b219752f67196568f23c7bc5cb5b2
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-24 17:38:42 -0700

    broken-on-6

--- server/getProduct.js
+++ server/getProduct.js
@@ -1,3 +1,16 @@
 const products = require('../products.json')
 
+const getProduct = (req, res) => {
+    const {id} = req.params
+    const item = products.find(
+        element => element.id === +id
+    )
+
+    if(item){
+        res.status(200).send(item)
+    } else {
+        res.status(500).send('Item not in list.')
+    }
+}
+
 module.exports = getProduct 
